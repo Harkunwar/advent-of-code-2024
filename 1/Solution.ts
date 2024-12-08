2402aa77e7baeeea94a0c1b4c3c3808e7032482b
Harkunwar Kochar <10580591+Harkunwar@users.noreply.github.com>
2024-12-08 10:51:51 -0800

    solving part 1 and 2

--- 1/Solution.ts
+++ 1/Solution.ts
@@ -1,17 +1,53 @@
 import fs from 'node:fs/promises';
+import path, { parse } from 'node:path';
 
-async function Part1() {
-    const fileInput = (await fs.readFile('./input.txt')).toString();
+
+async function parseInput(): Promise<{ left: number[], right: number[] }> {
+    const inputPath = path.resolve(import.meta.dirname, './input.txt');
+    const input = (await fs.readFile(inputPath)).toString();
+
     const left: number[] = [], right: number[] = [];
-    for (const line of fileInput.split('\n')) {
-        const [leftNumber, rightNumber] = line.split(' ').map(Number);
+    const lines = input.split('\n');
+    for (const line of lines) {
+        const [leftNumber, rightNumber] = line.split(/\s+/).map(number => Number(number));
         left.push(leftNumber);
         right.push(rightNumber);
     }
-    left.sort((a, b) => a-b);
-    right.sort((a, b) => a-b);
-    console.log(left);
+
+    return { left, right };
 }
 
+async function Part1() {
+    const { left, right } = await parseInput();
+    
+    left.sort((a, b) => a-b);
+    right.sort((a, b) => a-b);
+
+    let distance = 0;
+    for(let i=0; i<left.length; i++) {
+        distance += Math.abs(left[i]-right[i]);
+    }
+    console.log('Part 1:', distance);
+}
+
+async function Part2() {
+    const { left, right } = await parseInput();
+    const rightCounts = new Map<number, number>();
+    for (const number of right) {
+        const count = rightCounts.get(number) ?? 0;
+        rightCounts.set(number, count + 1); 
+    }
+
+    let similarityScore = 0;
+    for (const number of left) {
+        const counts = rightCounts.get(number) ?? 0;
+        similarityScore += number * counts;
+    }
+
+    console.log('Part 2:', similarityScore);
+}
+
+
 Part1();
+Part2();
 
--- flake.nix
+++ flake.nix
@@ -17,9 +17,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs_22
-            # Common development tools
             nodePackages.npm
-            nodePackages.yarn
             git
           ];
 
